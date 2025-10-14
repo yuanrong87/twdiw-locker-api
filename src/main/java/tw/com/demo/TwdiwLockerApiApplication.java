86d1a427f10d2a8639caa8f1a3addb07d1607ab1
Yuan <hihirandy@gmail.com>
2025-10-14 21:39:32 +0800

    init project

---
+++ src/main/java/tw/com/demo/TwdiwLockerApiApplication.java
@@ -0,0 +1,11 @@
+package tw.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwdiwLockerApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TwdiwLockerApiApplication.class, args);
+    }
+}
---
+++ src/main/java/tw/com/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package tw.com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @RequestMapping("/test")
+    public String test() {
+        return "success";
+    }
+}
